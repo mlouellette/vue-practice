394d08aa34333e523a814e8543c6b10d2de2b59b
mlouellette <112026857+mlouellette@users.noreply.github.com>
2023-08-29 21:35:36 -0400

    Checklist

--- js/components/AssignementList.js
+++ js/components/AssignementList.js
@@ -4,11 +4,25 @@ export default {
     components: { Assignement },
     template: `
         <section v-show="assignements.length">
-        <h2 class="font-bold mb-2">{{ title }}</h2>
+        <h2 class="font-bold mb-2">
+            {{ title }}
+            <span>({{ assignements.length }})</span>
+        </h2>
+
+        <div class="flex gap-2">
+            <button
+              @click="currentTag = tag"
+              v-for="tag in tags"
+              class="border rounded px-1 py-px text-xs"
+              :class="{
+                'border-blue-500 text-blue-500' : tag === currentTag
+              }"
+            >{{ tag }}</button>
+        </div>
         
-            <ul class="border border-gray-600 divide-y divide-gray-600">
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignement 
-                    v-for="assignement in assignements"
+                    v-for="assignement in filteredAssignments"
                     :key="assignement.id"
                     :assignement="assignement"
                 ></assignement>
@@ -20,5 +34,25 @@ export default {
     props: {
         assignements: Array,
         title: String
+    },
+
+    data() {
+        return {
+            currentTag: 'all'
+        };
+    },
+
+    computed: {
+        filteredAssignments() {
+            if (this.currentTag === 'all') {
+                return this.assignements;
+            }
+
+            return this.assignements.filter(a => a.tag === this.currentTag);
+        },
+
+        tags() {
+            return ['all', ...new Set(this.assignements.map(a => a.tag))];
+        }
     }
 }
--- js/components/Assignements.js
+++ js/components/Assignements.js
@@ -14,9 +14,9 @@ export default {
     data () {
         return {
             assignements: [
-               { name: 'Finish Project', complete: false, id: 1 },
-               { name: 'Read chapter 4', complete: false, id: 2 }, 
-               { name: 'Turn in homework', complete: false, id: 3 },  
+               { name: 'Finish Project', complete: false, id: 1, tag: 'math' },
+               { name: 'Read chapter 4', complete: false, id: 2 , tag: 'science'}, 
+               { name: 'Turn in homework', complete: false, id: 3, tag: 'math' },  
             ],
 
 
